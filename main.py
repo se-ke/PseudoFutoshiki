ad7fa7d3d2677af456ff7a85f99cc93cc837d5a2
Sean Kelly <skboogi@live.com>
2020-11-02 21:19:09 -0500

    got psbe to work

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ def s(x, y, z):
 
 
 def read_file(file_name):
-    with open(file_name, 'r') as f:
-        fic = f.read().split('\n')
-    return fic
+    return open(file_name, 'r').read().split('\n')
 
 
 def get_grid(f):
@@ -55,8 +53,9 @@ def get_clause(v, c):
     vpool = IDPool()
     for i in range(n):
         for j in range(n):
+            for z in range(1, n + 1):
+                vpool.id('v{0}'.format(s(i, j, z)))
             if v[i][j] != "_":
-                vpool.id('v{0}'.format(s(i, j, v[i][j])))
                 p.extend(PBEnc.equals(lits=[s(i, j, v[i][j])], bound=1, vpool=vpool).clauses)
     # one value per square
     for x in range(n):
@@ -65,36 +64,21 @@ def get_clause(v, c):
             for z in range(1, n + 1):
                 lits.append(s(x, y, z))
             p.extend(PBEnc.equals(lits=lits, bound=1, vpool=vpool).clauses)
-    # one value per line
-    for y in range(n):
-        lits = []
-        weights = []
-        for z in range(1, n + 1):
-            for x in range(n):
-                lits.append(s(x, y, z)) # row 1, column 1 -> row 2 column 1 -> row 3 column 1 -> ... -> row n column 1
-                weights.append(z)
-                #lits2.append(s(y, x, z)) # row 1, column 1 -> row 1 column 2 -> row 1 column 3 -> ... -> row 1 column n
-        p.extend(PBEnc.equals(lits=lits, weights=weights, bound=int((n * (n + 1) / 2)), vpool=vpool).clauses)
-    # 2 3 1
-    # 2 2 2
-    # 2 2 2
-    # one value per column
-    for x in range(n):
-        lits = []
-        weights = []
-        for z in range(1, n + 1):
-            for y in range(n):
-                lits.append(s(x, y, z))
-                weights.append(z)
-        p.extend(PBEnc.equals(lits=lits, weights=weights, bound=int((n * (n + 1) / 2)), vpool=vpool).clauses)
-    # 3 2 1
-    # 2 2 2
-    # 1 2 3
+    # unique values in each row or column
+    for z in range(1, n + 1):
+        for a in range(n):
+            lits_row = []
+            lits_col = []
+            for b in range(n):
+                lits_row.append(s(a, b, z))
+                lits_col.append(s(b, a, z))
+            p.extend(PBEnc.equals(lits=lits_row, bound=1, vpool=vpool).clauses)
+            p.extend(PBEnc.equals(lits=lits_col, bound=1, vpool=vpool).clauses)
     # inequalities hold
     for x in c:
-        (a, b) = x        # x1+x2+x3 - (y1+y2+y3) > 0
-        (i1, j1) = a      # x1+x2+x3 > y1+y2+y3
-        (i2, j2) = b      # X > Y
+        (a, b) = x
+        (i1, j1) = a
+        (i2, j2) = b
         lits = []
         weights = []
         for z in range(1, n + 1):
@@ -104,15 +88,6 @@ def get_clause(v, c):
             weights.append(-z)
         p.extend(PBEnc.atleast(lits=lits, weights=weights, bound=1, vpool=vpool).clauses)
     return p
-    # [[2, 1], [-2, -1], [4, 3], [-4, -3], [6, 5], [-6, -5], [8, 7], [-8, -7], [9], [-10, 1], [-11, -5, -1],
-    # [-11, 5, 10], [-11, -1, 10], [-12, -2], [-12, 2, 11], [-12, 11], [-13, -2, 11], [-13, 2], [-13, 11],
-    # [-14, -6, 12], [-14, 6, 13], [-14, 12, 13], [14], [15], [-16, 3], [-17, -7, -3], [-17, 7, 16],
-    # [-17, -3, 16], [-18, -4], [-18, 4, 17], [-18, 17], [-19, -4, 17], [-19, 4], [-19, 17],
-    # [-20, -8, 18], [-20, 8, 19], [-20, 18, 19], [20], [21], [-22, 1], [-23, -3, -1],
-    # [-23, 3, 22], [-23, -1, 22], [-24, -2], [-24, 2, 23], [-24, 23], [-25, -2, 23],
-    # [-25, 2], [-25, 23], [-26, -4, 24], [-26, 4, 25], [-26, 24, 25], [26], [27], [-28, 5],
-    # [-29, -7, -5], [-29, 7, 28], [-29, -5, 28], [-30, -6], [-30, 6, 29], [-30, 29], [-31, -6, 29],
-    # [-31, 6], [-31, 29], [-32, -8, 30], [-32, 8, 31], [-32, 30, 31], [32], [3], [], [-8]]
 
 
 def solve(p):
@@ -138,21 +113,6 @@ def print_sol(sol):
 fic = read_file("boards/grid3_1")
 v, c = get_grid(fic)
 p = get_clause(v, c)
-print(p.clauses)
+print(len(p.clauses))
 solve(p)
 
-# 1 2 3
-# 3 1 2
-# 2 3 1
-
-# 2 3 1
-# 1 2 3
-# 3 1 2
-
-# 2 3 1
-# 3 1 2
-# 1 2 3
-
-# 2 3 1
-# 2 1 3
-# 2 2 2
